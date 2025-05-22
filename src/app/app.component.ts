48a8b75fa8e5d133e814292b987824304866e724
Philipp Lötszch <philipp.loetzsch@outlook.com>
2025-05-22 20:29:09 +0200

    style buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MainSiteComponent } from "./components/main-site/main-site.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainSiteComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
