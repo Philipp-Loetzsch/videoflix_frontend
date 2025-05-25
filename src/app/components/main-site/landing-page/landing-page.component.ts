8ec6403d31acb85e247fe54bff41b5df4276a9d0
Philipp Lötszch <philipp.loetzsch@outlook.com>
2025-05-25 20:02:43 +0200

    landing page logic

--- src/app/components/main-site/landing-page/landing-page.component.ts
+++ src/app/components/main-site/landing-page/landing-page.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrl: './landing-page.component.scss',
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
+  loginForm = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+      Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+    ]),
+  });
 
+  ngOnInit(): void {
+    this.loginForm.setValue({ email: '' });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('E-Mail:', this.loginForm.value.email);
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
+    get email(): FormControl {
+    return this.loginForm.get('email') as FormControl;
+  }
 }
--- src/app/components/main-site/main-site.component.ts
+++ src/app/components/main-site/main-site.component.ts
@@ -1,13 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-main-site',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './main-site.component.html',
   styleUrl: './main-site.component.scss'
 })
 export class MainSiteComponent {
+  backgroundClass = '';
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      const path = this.router.url;
+      if (path.includes('')) {
+        this.backgroundClass = 'landing-bg';
+      } else if (path.includes('log_in')) {
+        this.backgroundClass = 'login-bg';
+      } else if (path.includes('sign_in')) {
+        this.backgroundClass = 'signin-bg';
+      } else if (path.includes('forgot_password')) {
+        this.backgroundClass = 'forgot-bg';
+      } else {
+        this.backgroundClass = 'default-bg';
+      }
+    });
+  }
+
 
 }
