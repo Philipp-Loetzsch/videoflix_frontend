2c2c6c2858ba8b4d529c3f88d896cc86c6361bcb
Philipp Lötszch <philipp.loetzsch@outlook.com>
2025-05-28 19:58:06 +0200

    sign in validation

--- src/app/components/main-site/sign-up/sign-up.component.ts
+++ src/app/components/main-site/sign-up/sign-up.component.ts
@@ -1,12 +1,97 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormControl,
+  FormGroup,
+  Validators,
+  AbstractControl,
+  ValidationErrors,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.scss'
+  styleUrl: './sign-up.component.scss',
 })
 export class SignUpComponent {
+  constructor(private router: Router) {}
 
+  showPw: boolean = false;
+  showCpw: boolean = false;
+  submitted: boolean = false;
+  emailError: string = '';
+  pwError: string = '';
+  cpwError: string = '';
+
+  signinForm = new FormGroup(
+    {
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      confirmpassword: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    },
+    { validators: this.passwordsMatch }
+  );
+
+  get email(): FormControl {
+    return this.signinForm.get('email') as FormControl;
+  }
+  get password(): FormControl {
+    return this.signinForm.get('password') as FormControl;
+  }
+  get confirmpassword(): FormControl {
+    return this.signinForm.get('confirmpassword') as FormControl;
+  }
+
+  passwordsMatch(group: AbstractControl): { [key: string]: boolean } | null {
+    const password = group.get('password')?.value;
+    const confirm = group.get('confirmpassword')?.value;
+    return password === confirm ? null : { passwordMismatch: true };
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.signinForm.valid) {
+      console.log('E-Mail:', this.signinForm.value.email);
+      this.router.navigate(['/sign_up']);
+    } else this.errorMsg();
+  }
+
+  errorMsg() {
+    const emailErrors = this.email.errors;
+    const pwErrors = this.password.errors;
+    if (emailErrors) {
+      if (emailErrors['required']) this.emailError = 'Please enter an Email';
+      else if (emailErrors['email'] || emailErrors['pattern']) {
+        this.emailError = 'Please enter a valid Email. E.g. your-mail@example.com';
+      }
+    }
+    else this.emailError = ''
+    if (pwErrors) this.errorPwMsg(pwErrors)
+      else this.pwError= ''
+  }
+
+  errorPwMsg(pwErrors : ValidationErrors){
+    if(pwErrors['required']) this.pwError = 'please enter a password';
+    else if (pwErrors['minlength']) {
+      this.pwError = 'minimum lenght 6';
+    }
+  }
+
+  togglePw(field: 'showPw' | 'showCpw') {
+    this[field] = !this[field];
+  }
 }
