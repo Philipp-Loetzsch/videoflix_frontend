f12169f8ec666ebc00fe8971870eccf61525bfda
Philipp Lötszch <philipp.loetzsch@outlook.com>
2025-07-23 20:19:35 +0200

    link to laws

--- src/app/components/law/law.component.ts
+++ src/app/components/law/law.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-law',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './law.component.html',
   styleUrl: './law.component.scss'
 })
